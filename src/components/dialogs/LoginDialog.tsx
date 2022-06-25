2be61bc99d9bead2d3ea294d463e01f2c5b29d45
liutydm <dmytro.liutyi@bluejeans.com>
2022-06-26 00:12:02 +0300

    add formik for form validation

--- src/components/dialogs/LoginDialog.tsx
+++ src/components/dialogs/LoginDialog.tsx
@@ -1,4 +1,5 @@
 import {
+  Box,
   Button,
   Dialog,
   DialogActions,
@@ -8,11 +9,21 @@ import {
   InputAdornment,
   TextField,
 } from "@mui/material";
-import React, { useContext, useState } from "react";
+import React, { useContext } from "react";
 import PersonRoundedIcon from "@mui/icons-material/PersonRounded";
 import KeyRoundedIcon from "@mui/icons-material/KeyRounded";
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import { NotificationContext } from "../contexts/NotificationContext";
+import * as yup from "yup";
+import { useFormik } from "formik";
+
+const validationSchema = yup.object({
+  email: yup
+    .string()
+    .email("Enter a valid email")
+    .required("Email is required"),
+  password: yup.string().required("Password is required"),
+});
 
 interface Props {
   isOpen: boolean;
@@ -20,91 +31,110 @@ interface Props {
 }
 
 const LoginDialog: React.FC<Props> = ({ isOpen, onClose }) => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
-  const onEmailChanged = (e: any) => setEmail(e.target.value);
-  const onPasswordChanged = (e: any) => setPassword(e.target.value);
-
   const auth = getAuth();
   const { setNotification } = useContext(NotificationContext);
 
-  const onSubmit = async () => {
-    try {
-      await signInWithEmailAndPassword(auth, email, password);
-    } catch (e: unknown) {
-      if (e instanceof Error) {
-        setNotification({
-          value: e.message,
-          type: "error",
-        });
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    validationSchema: validationSchema,
+    onSubmit: async (values) => {
+      try {
+        await signInWithEmailAndPassword(auth, values.email, values.password);
+      } catch (e: unknown) {
+        if (e instanceof Error) {
+          setNotification({
+            value: e.message,
+            type: "error",
+          });
+        }
       }
-    }
-  };
+    },
+  });
 
   return (
     <Dialog fullWidth open={isOpen} onClose={onClose}>
-      <DialogTitle textAlign="center" variant="h5" mt={2}>
-        Login
-      </DialogTitle>
-      <DialogContent>
-        <Grid container flexDirection="column" spacing={2} mt={1} px={3}>
-          <Grid item>
-            <TextField
-              required
-              fullWidth
-              id="email"
-              type="email"
-              label="Email"
-              variant="outlined"
-              value={email}
-              onChange={onEmailChanged}
-              InputProps={{
-                startAdornment: (
-                  <InputAdornment position="start">
-                    <PersonRoundedIcon />
-                  </InputAdornment>
-                ),
-              }}
-            />
-          </Grid>
-          <Grid item>
-            <TextField
-              required
-              fullWidth
-              id="password"
-              type="password"
-              label="Password"
-              variant="outlined"
-              value={password}
-              onChange={onPasswordChanged}
-              InputProps={{
-                startAdornment: (
-                  <InputAdornment position="start">
-                    <KeyRoundedIcon />
-                  </InputAdornment>
-                ),
-              }}
-            />
-          </Grid>
-        </Grid>
-      </DialogContent>
-      <DialogActions>
-        <Grid container mb={5} px={3} spacing={2}>
-          <Grid item container spacing={1}>
-            <Grid item xs={10}>
-              <Button fullWidth onClick={onSubmit} variant="outlined">
-                Sign In
-              </Button>
+      <Box
+        component="form"
+        noValidate
+        onSubmit={(e: any) => {
+          e.preventDefault();
+          formik.submitForm();
+        }}
+      >
+        <DialogTitle textAlign="center" variant="h5" mt={2}>
+          Login
+        </DialogTitle>
+        <DialogContent>
+          <Grid container flexDirection="column" spacing={2} mt={1} px={3}>
+            <Grid item>
+              <TextField
+                required
+                fullWidth
+                id="email"
+                type="email"
+                label="Email"
+                variant="outlined"
+                {...formik.getFieldProps("email")}
+                error={formik.touched.email && Boolean(formik.errors.email)}
+                helperText={formik.touched.email && formik.errors.email}
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <PersonRoundedIcon />
+                    </InputAdornment>
+                  ),
+                }}
+              />
             </Grid>
-            <Grid item xs={2}>
-              <Button fullWidth onClick={onClose} variant="text">
-                Close
-              </Button>
+            <Grid item>
+              <TextField
+                required
+                fullWidth
+                id="password"
+                type="password"
+                label="Password"
+                variant="outlined"
+                {...formik.getFieldProps("password")}
+                error={
+                  formik.touched.password && Boolean(formik.errors.password)
+                }
+                helperText={formik.touched.password && formik.errors.password}
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <KeyRoundedIcon />
+                    </InputAdornment>
+                  ),
+                }}
+              />
             </Grid>
           </Grid>
-        </Grid>
-      </DialogActions>
+        </DialogContent>
+        <DialogActions>
+          <Grid container mb={5} px={3} spacing={2}>
+            <Grid item container spacing={1}>
+              <Grid item xs={10}>
+                <Button
+                  type="submit"
+                  fullWidth
+                  disabled={formik.isSubmitting || !formik.isValid}
+                  variant="outlined"
+                >
+                  Sign In
+                </Button>
+              </Grid>
+              <Grid item xs={2}>
+                <Button fullWidth onClick={onClose} variant="text">
+                  Close
+                </Button>
+              </Grid>
+            </Grid>
+          </Grid>
+        </DialogActions>
+      </Box>
     </Dialog>
   );
 };
--- src/components/dialogs/RegistrationDialog.tsx
+++ src/components/dialogs/RegistrationDialog.tsx
@@ -1,4 +1,5 @@
 import {
+  Box,
   Button,
   Dialog,
   DialogActions,
@@ -8,7 +9,7 @@ import {
   InputAdornment,
   TextField,
 } from "@mui/material";
-import React, { useContext, useState } from "react";
+import React, { useContext } from "react";
 import KeyRoundedIcon from "@mui/icons-material/KeyRounded";
 import EmailRoundedIcon from "@mui/icons-material/EmailRounded";
 import PersonRoundedIcon from "@mui/icons-material/PersonRounded";
@@ -19,123 +20,167 @@ import {
 } from "firebase/auth";
 import { NotificationContext } from "../contexts/NotificationContext";
 import { UserContext } from "../contexts/UserContext";
+import * as yup from "yup";
+import { useFormik } from "formik";
 
 interface Props {
   isOpen: boolean;
   onClose: () => void;
 }
 
+const validationSchema = yup.object({
+  email: yup
+    .string()
+    .email("Enter a valid email")
+    .required("Email is required"),
+  nickname: yup
+    .string()
+    .min(3, "Nickname should be of minimum 3 characters")
+    .required("Nickname is required"),
+  password: yup
+    .string()
+    .min(6, "Password should be of minimum 6 characters")
+    .required("Password is required"),
+});
+
 const RegistrationDialog: React.FC<Props> = ({ isOpen, onClose }) => {
-  const [email, setEmail] = useState("");
-  const [nickname, setNickname] = useState("");
-  const [password, setPassword] = useState("");
-
-  const onEmailChanged = (e: any) => setEmail(e.target.value);
-  const onNicknameChanged = (e: any) => setNickname(e.target.value);
-  const onPasswordChanged = (e: any) => setPassword(e.target.value);
-
   const auth = getAuth();
   const { updateUser } = useContext(UserContext);
   const { setNotification } = useContext(NotificationContext);
 
-  const onSubmit = async () => {
-    try {
-      await createUserWithEmailAndPassword(auth, email, password);
-      await updateProfile(auth.currentUser!, { displayName: nickname });
-      console.log(auth.currentUser);
-      updateUser({ displayName: nickname });
-    } catch (e: unknown) {
-      if (e instanceof Error) {
-        setNotification({
-          value: e.message,
-          type: "error",
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      nickname: "",
+      password: "",
+    },
+    validationSchema: validationSchema,
+    onSubmit: async (values) => {
+      try {
+        await createUserWithEmailAndPassword(
+          auth,
+          values.email,
+          values.password
+        );
+        await updateProfile(auth.currentUser!, {
+          displayName: values.nickname,
         });
+        console.log(auth.currentUser);
+        updateUser({ displayName: values.nickname });
+      } catch (e: unknown) {
+        if (e instanceof Error) {
+          setNotification({
+            value: e.message,
+            type: "error",
+          });
+        }
       }
-    }
-  };
+    },
+  });
 
   return (
     <Dialog fullWidth open={isOpen} onClose={onClose}>
-      <DialogTitle textAlign="center" variant="h5" mt={2}>
-        Login
-      </DialogTitle>
-      <DialogContent>
-        <Grid container flexDirection="column" spacing={2} mt={1} px={3}>
-          <Grid item>
-            <TextField
-              required
-              fullWidth
-              id="email"
-              type="email"
-              label="Email"
-              variant="outlined"
-              value={email}
-              onChange={onEmailChanged}
-              InputProps={{
-                startAdornment: (
-                  <InputAdornment position="start">
-                    <EmailRoundedIcon />
-                  </InputAdornment>
-                ),
-              }}
-            />
-          </Grid>
-          <Grid item>
-            <TextField
-              required
-              fullWidth
-              id="nickname"
-              type="text"
-              label="Nickname"
-              variant="outlined"
-              value={nickname}
-              onChange={onNicknameChanged}
-              InputProps={{
-                startAdornment: (
-                  <InputAdornment position="start">
-                    <PersonRoundedIcon />
-                  </InputAdornment>
-                ),
-              }}
-            />
-          </Grid>
-          <Grid item>
-            <TextField
-              required
-              fullWidth
-              id="password"
-              type="password"
-              label="Password"
-              variant="outlined"
-              value={password}
-              onChange={onPasswordChanged}
-              InputProps={{
-                startAdornment: (
-                  <InputAdornment position="start">
-                    <KeyRoundedIcon />
-                  </InputAdornment>
-                ),
-              }}
-            />
-          </Grid>
-        </Grid>
-      </DialogContent>
-      <DialogActions>
-        <Grid container mb={5} px={3} spacing={2}>
-          <Grid item container spacing={1}>
-            <Grid item xs={10}>
-              <Button fullWidth onClick={onSubmit} variant="outlined">
-                Sign In
-              </Button>
+      <Box
+        component="form"
+        noValidate
+        onSubmit={(e: any) => {
+          e.preventDefault();
+          formik.submitForm();
+        }}
+      >
+        <DialogTitle textAlign="center" variant="h5" mt={2}>
+          Registration
+        </DialogTitle>
+        <DialogContent>
+          <Grid container flexDirection="column" spacing={2} mt={1} px={3}>
+            <Grid item>
+              <TextField
+                required
+                fullWidth
+                id="email"
+                type="email"
+                label="Email"
+                variant="outlined"
+                {...formik.getFieldProps("email")}
+                error={formik.touched.email && Boolean(formik.errors.email)}
+                helperText={formik.touched.email && formik.errors.email}
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <EmailRoundedIcon />
+                    </InputAdornment>
+                  ),
+                }}
+              />
             </Grid>
-            <Grid item xs={2}>
-              <Button fullWidth onClick={onClose} variant="text">
-                Close
-              </Button>
+            <Grid item>
+              <TextField
+                required
+                fullWidth
+                id="nickname"
+                type="text"
+                label="Nickname"
+                variant="outlined"
+                {...formik.getFieldProps("nickname")}
+                error={
+                  formik.touched.nickname && Boolean(formik.errors.nickname)
+                }
+                helperText={formik.touched.nickname && formik.errors.nickname}
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <PersonRoundedIcon />
+                    </InputAdornment>
+                  ),
+                }}
+              />
+            </Grid>
+            <Grid item>
+              <TextField
+                required
+                fullWidth
+                id="password"
+                type="password"
+                label="Password"
+                variant="outlined"
+                {...formik.getFieldProps("password")}
+                error={
+                  formik.touched.password && Boolean(formik.errors.password)
+                }
+                helperText={formik.touched.password && formik.errors.password}
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <KeyRoundedIcon />
+                    </InputAdornment>
+                  ),
+                }}
+              />
             </Grid>
           </Grid>
-        </Grid>
-      </DialogActions>
+        </DialogContent>
+        <DialogActions>
+          <Grid container mb={5} px={3} spacing={2}>
+            <Grid item container spacing={1}>
+              <Grid item xs={10}>
+                <Button
+                  type="submit"
+                  fullWidth
+                  disabled={formik.isSubmitting || !formik.isValid}
+                  variant="outlined"
+                >
+                  Sign Up
+                </Button>
+              </Grid>
+              <Grid item xs={2}>
+                <Button fullWidth onClick={onClose} variant="text">
+                  Close
+                </Button>
+              </Grid>
+            </Grid>
+          </Grid>
+        </DialogActions>
+      </Box>
     </Dialog>
   );
 };
